6acda8e0ff0d53b9124739987756606206beeea5
fatih ozer <fatihozer246@hotmail.com>
2022-10-07 19:22:07 +0200

    added c03, c04, c05

---
+++ src/main/java/day02_driverMethods/C05_DriverManageMethods.java
@@ -0,0 +1,41 @@
+package day02_driverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C05_DriverManageMethods {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        /*
+        Waits method will be explained further.
+        Implicitly wait, driverin aradigi elementleri bulabilmesi icin kendisine verdigimiz arastirma suresidir
+        O sure icerisinde aradigi elementi bulamazsa hata verir, ama bu sureyi beklemek zorunda degildir.
+        Bu maksimum bekleme suresidir.
+         */
+
+        driver.manage().window().fullscreen();
+        System.out.println("Fullscreen position : "+driver.manage().window().getPosition());
+        System.out.println("Fullscreen size : "+driver.manage().window().getSize());
+
+        Thread.sleep(2000);
+
+        driver.manage().window().maximize();
+        System.out.println("Maximize position : "+driver.manage().window().getPosition());
+        System.out.println("Maximize size : "+driver.manage().window().getSize());
+
+        Thread.sleep(2000);
+
+        driver.manage().window().minimize();
+        System.out.println("Minimize position : "+driver.manage().window().getPosition());
+        System.out.println("Minimize size : "+driver.manage().window().getSize());
+
+
+        // driver.close();
+    }
+}
